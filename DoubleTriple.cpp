84cbd5c9709d5969540ee73a399564f19ea5869b
Mohammad <m.mirgolbabaei666@gmail.com>
2023-04-19 09:40:45 +0330

    Create DoubleTriple.cpp

---
+++ DoubleTriple.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+main(){
+	cout<<"Enter number: ";
+	int x;
+	cin>>x;
+	cout<<"Double = "<<x*2<<endl;
+	cout<<"Triple = "<<x*3;
+	getch();
+}
